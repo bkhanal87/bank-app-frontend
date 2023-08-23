e3981a38e2f685f4bb51085b3fef743b6ee2b70b
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-08-22 21:19:12 -0400

    create service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
@@ -12,7 +12,8 @@ import { UserListComponent } from './user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-user-list',
@@ -10,36 +12,17 @@ export class UserListComponent implements OnInit{
 
   users!: User[];
 
-  constructor() {
+  constructor(private userService: UserService) {
 
   }
 
   ngOnInit(): void {
-    this.users = [
-      {
-        id: 1,
-        name: 'Pete',
-        address: '123 Main Street',
-        phoneNumber: '000-000-0000',
-      },
-      {
-        id: 2,
-        name: 'Tom',
-        address: '789 Anystreet Dr',
-        phoneNumber: '000-000-0000',
-      },
-      {
-        id: 3,
-        name: 'Mike',
-        address: '824 Hamilton Ave',
-        phoneNumber: '000-000-0000',
-      },
-      {
-        id: 4,
-        name: 'John',
-        address: '567 Mason Ave',
-        phoneNumber: '000-000-0000',
-      },
-    ];
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.userService.getUserList().subscribe(data => {
+      this.users = data;
+    });
   }
 }
