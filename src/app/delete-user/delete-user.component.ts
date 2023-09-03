6129db5f944bddeaa889ad90d65eb886714f5f84
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-09-02 21:35:57 -0400

    update ui

--- src/app/delete-user/delete-user.component.ts
+++ src/app/delete-user/delete-user.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-delete-user',
   templateUrl: './delete-user.component.html',
-  styleUrls: ['./delete-user.component.css']
+  styleUrls: ['./delete-user.component.css'],
 })
-export class DeleteUserComponent {
-  
+export class DeleteUserComponent implements OnInit {
+  id!: number;
+  user: User = new User();
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.userService.getUserById(this.id).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => console.log(error)
+    );
+  }
+  onSubmit() {
+    this.userService.deleteUser(this.id).subscribe
+      (data => {
+        console.log(data);
+      },
+      error => console.log(error));
+  }
 }
