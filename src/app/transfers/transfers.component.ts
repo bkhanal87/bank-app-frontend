8b267717dcf5d38755675a2b9f4869f0158fa0e9
bkhanal87@gmail.com <bkhanal87@gmail.com>
2026-07-11 05:02:03 -0400

    change layout to include other pages

---
+++ src/app/transfers/transfers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfers',
+  templateUrl: './transfers.component.html',
+  styleUrls: ['./transfers.component.css']
+})
+export class TransfersComponent {
+
+}
